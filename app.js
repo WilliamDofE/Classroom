d7007f7e10c8eda23d705149a111886281049615
WilliamDofE <47364073+WilliamDofE@users.noreply.github.com>
2019-03-09 20:12:23 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+var io = require('socket.io')(serv);
+var path = require('path');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
  
-app.get('/',function(req, res) {
-    res.sendFile(__dirname + '/client/index.html');
+app.use(express.static('client/home'));
+
+app.post('/createSchool', function(req, res) {
+	response = {
+		school_name: req.body.schoolName,
+		school_private: req.body.schoolPrivate,
+		entry_password: req.body.entryPassword
+	};
+	console.log(response);
+  
+  
+  res.sendFile(path.join(__dirname, '/client/school/index.html'));
+  
+  io.emit('updateSchool', response);
 });
-app.use(express.static(__dirname + '/client'));
  
 serv.listen(3000);
-console.log("Server started.");
+console.log("Server started on localhost://3000");
