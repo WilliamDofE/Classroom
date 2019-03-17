4f3a74c18949c926037ef48efeba1dc753f7d385
WilliamDofE <47364073+WilliamDofE@users.noreply.github.com>
2019-03-17 21:20:56 +0000

    Update app.js
    I added some code so I can send data to the school creation page.

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ var path = require('path');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+var response;
  
+app.use('/home', express.static('client/home'));
+app.use('/school', express.static('client/school'));
+app.use('/images', express.static('client/assets/images'));
+
 app.use(express.static('client/home'));
 
 app.post('/createSchool', function(req, res) {
@@ -18,11 +24,15 @@ app.post('/createSchool', function(req, res) {
 	};
 	console.log(response);
   
-  
+  res.send(response);
   res.sendFile(path.join(__dirname, '/client/school/index.html'));
-  
-  io.emit('updateSchool', response);
 });
- 
+
+io.on('connection', (socket) => {
+  socket.on('pageReady', function() {
+    socket.emit('pageInfo', response);
+    console.log('page ready recieved');
+  });
+});
 serv.listen(3000);
 console.log("Server started on localhost://3000");
