071c50108d92e16232f8cdb460f7fddcd0ac3cb3
WilliamDofE <47364073+WilliamDofE@users.noreply.github.com>
2019-05-15 20:59:45 +0100

    Update app.js
    Added different pages for teachers and students, and added functionality to receive the data from the teacher's canvas and send it to the server, and from there to the student's page.

--- app.js
+++ app.js
@@ -8,31 +8,72 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-var response;
+let schoolResponse;
+let lessonResponse;
  
 app.use('/home', express.static('client/home'));
 app.use('/school', express.static('client/school'));
+app.use('/teacherLesson', express.static('client/teacherLesson'));
+app.use('/studentLesson', express.static('client/studentLesson'));
 app.use('/images', express.static('client/assets/images'));
+app.use('/literallycanvas', express.static('client/literallycanvas'));
 
 app.use(express.static('client/home'));
 
 app.post('/createSchool', function(req, res) {
-	response = {
+	schoolResponse = {
 		school_name: req.body.schoolName,
 		school_private: req.body.schoolPrivate,
 		entry_password: req.body.entryPassword
 	};
-	console.log(response);
+	console.log(schoolResponse);
   
   res.sendFile(path.join(__dirname, '/client/school/index.html'));
 });
 
+app.post('/teacherLesson', function(req, res) {
+	lessonResponse = {
+		lesson_name: req.body.lessonName,
+		lesson_desc: req.body.lessonDesc,
+		lesson_sub: req.body.lessonSubject,
+    lesson_private: req.body.lessonPrivate,
+    lesson_password: req.body.entryPassword,
+    lesson_teacher: req.body.lessonTeacher
+	};
+	console.log(lessonResponse);
+  
+  res.sendFile(path.join(__dirname, '/client/teacherLesson/index.html'));
+});
+
+app.post('/studentLesson', function(req, res) {
+	lessonResponse = {
+		lesson_name: req.body.lessonName,
+		lesson_desc: req.body.lessonDesc,
+		lesson_sub: req.body.lessonSubject,
+    lesson_private: req.body.lessonPrivate,
+    lesson_password: req.body.entryPassword,
+    lesson_teacher: req.body.lessonTeacher
+	};
+	console.log(lessonResponse);
+  
+  res.sendFile(path.join(__dirname, '/client/studentLesson/index.html'));
+});
+
 io.on('connection', (socket) => {
   console.log('connection! hallelujah this is taking forever');
-  socket.on('pageReady', function() {
-    socket.emit('pageInfo', response);
+  socket.on('schoolPageReady', function() {
+    socket.emit('schoolPageInfo', schoolResponse);
+    console.log('school page ready recieved');
   });
-  console.log('page ready recieved');
+  socket.on('lessonPageReady', function() {
+    socket.emit('lessonPageInfo', lessonResponse);
+    console.log('lesson page ready recieved');
+  });
+  socket.on('updateLessonCanvas', function(lessonCanvasState) {
+    io.emit('lessonCanvasUpdate', lessonCanvasState);
+    console.log('lesson canvas update recieved');
+    console.log(lessonCanvasState);
+  });
 });
 
 serv.listen(3000);
